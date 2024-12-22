22856df00131829f534f4bc5e5f9e0788cbcc885
Shrey Singh <144680714+ShreySigh2912@users.noreply.github.com>
2024-12-22 16:20:32 +0530

    Add main App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoApp from './components/TodoApp';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
